debf5014bb4d5442bcd4949cd68a0f616bbdb763
Adam Kariv <adam.kariv@gmail.com>
2021-12-06 16:33:50 +0200

    + business-licensing-items-by-property taxonomy

---
+++ taxonomies/business-licensing-items/by-property/process.py
@@ -0,0 +1,51 @@
+from dataflows import Flow, set_type
+
+from dgp.core.base_enricher import ColumnTypeTester, ColumnReplacer, \
+        DatapackageJoiner, enrichments_flows, BaseEnricher
+from dgp.config.consts import RESOURCE_NAME
+
+from datacity_server.processors import MunicipalityNameToCodeEnricher, \
+    FilterEmptyFields, StreamingDuplicateRemover
+
+
+class FilterEmptyCodes(FilterEmptyFields):
+    FIELDS_TO_CHECK = {
+        'property-code': None
+    }
+
+class LicensingItemCodeFixer(BaseEnricher):
+    def __init__(self, *args):
+        super().__init__(*args)
+
+    def test(self):
+        return True
+
+    def fix_code(self, code):
+        if not code:
+            return None
+        code = str(code).strip()
+        while len(code) < 8:
+            code = '0' + code
+        parts = []
+        while len(code) > 0:
+            parts.append(int(code[:2]))
+            code = code[2:]
+        return '.'.join(map(str, parts))
+
+    def work(self):
+        return Flow(
+            set_type('property-code', 'string', transform=lambda v: self.fix_code(v), resources=RESOURCE_NAME),
+        )
+
+    def postflow(self):
+        return Flow(self.work())
+
+
+def flows(config, context):
+    return enrichments_flows(
+        config, context,
+        MunicipalityNameToCodeEnricher,
+        FilterEmptyCodes,
+        LicensingItemCodeFixer,
+        StreamingDuplicateRemover,
+    )
