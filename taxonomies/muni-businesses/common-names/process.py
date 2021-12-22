110705c22da625e2cc1cfdf35f000f69211a79aa
Adam Kariv <adam.kariv@gmail.com>
2021-12-22 10:59:57 +0200

    Enumerate business kinds

--- taxonomies/muni-businesses/common-names/process.py
+++ taxonomies/muni-businesses/common-names/process.py
@@ -1,4 +1,6 @@
-from dgp.core.base_enricher import enrichments_flows
+import dataflows as DF
+
+from dgp.core.base_enricher import enrichments_flows, BaseEnricher
 from dgp.config.consts import RESOURCE_NAME
 
 from datacity_server.processors import MunicipalityNameToCodeEnricher, FilterEmptyFields
@@ -10,10 +12,29 @@ class FilterEmptyCodes(FilterEmptyFields):
         'business-kind-internal': None,
     }
 
+class Enumerator(BaseEnricher):
+
+    def test(self):
+        return True
+
+    def work(self):
+        def func(rows):
+            for i, row in enumerate(rows):
+                row['business-kind-order'] = i
+                yield row
+        return func
+
+    def postflow(self):
+        return DF.Flow(
+            DF.add_field('business-kind-order', 'integer', 0),
+            self.work()
+        )
+
 
 def flows(config, context):
     return enrichments_flows(
         config, context,
         MunicipalityNameToCodeEnricher,
         FilterEmptyCodes,
+        Enumerator,
     )
